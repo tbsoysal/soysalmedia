01f57b0546d5976fe5fa1874be9bbb2f187e2503
Tahaberk SOYSAL <berksysl.g@gmail.com>
2025-08-24 15:35:07 +0300

    change testimonials section

--- src/components/testimonials/index.tsx
+++ src/components/testimonials/index.tsx
@@ -2,19 +2,19 @@ const Testimonials = () => {
   const logos = [
     "./kilincyapi.png",
     "./peracity.png",
-    "./buyukardahan.png"
+    "./grandardahan.png"
   ]
   return (
     <section className="mb-10 md:mb-11 lg:mb-[102px]">
       <h2 className="text-white text-center text-[18px] md:text-[32px] font-medium mb-8 md:mb-10 lg:mb-[64px]">Yüzlerce işletme bize güveniyor.</h2>
-      <div className="flex overflow-hidden max-w-[700px] mx-auto">
+      <div className="flex overflow-hidden max-w-[100vw] mx-auto">
         <div className="shrink-0 flex justify-between items-center gap-3 animate-scroll">
-          {[...logos, ...logos, ...logos].map((logo, id) => (
+          {[...logos, ...logos, ...logos, ...logos, ...logos, ...logos].map((logo, id) => (
             <img
               key={id}
               src={logo}
               alt={`logo-${id}`}
-              className="h-12 inline-block"
+              className="h-30 inline-block"
             />
           ))}
         </div>
